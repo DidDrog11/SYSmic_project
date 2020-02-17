1ab955f5b72bf4459654ed99b12739bef5260453
DidDrog11 <dsimons1987@gmail.com>
2020-02-17 15:29:06 +0000

    Created a model that allows replication of the cyclical pattern of measles epidemics based on births into the susceptible compartment

--- Miniproject.R
+++ Miniproject.R
@@ -1,7 +1,10 @@
 library("deSolve") ##Package to numerically solve differential equations
 library("tidyverse")
 library("patchwork")
+library("socialmixr")
 
+
+# Part 1 ------------------------------------------------------------------
 ##Setting model parameters
 R_0 <- 12 ##The number of infections caused by a single introduction of a case into a susceptible population
 N <- 10000 ##The population at the beginning of the model
@@ -62,10 +65,10 @@ SIR_2 <- as.data.frame(ode(y = init,
 plot_2 <- plot_graph(SIR_2)
 
 ##Adjusting the model to reflect a lower limit for infectious period
-R_0 <- 12 ##This is the upper limit from the published literature
+R_0 <- 12 ##No change
 N <- 10000 ##No change
 I <- 12  ##No change
-tau <- 7 ##No change
+tau <- 7 ##Lower limit of infectious period
 
 gamma = gamma_val(tau) ##No change
 beta = beta_val(R_0, N, gamma) ##This will change based on the value of R0
@@ -87,10 +90,10 @@ SIR_3 <- as.data.frame(ode(y = init,
 plot_3 <- plot_graph(SIR_3)
 
 ##Adjusting the model to reflect an upper limit for infectious period
-R_0 <- 12 ##This is the upper limit from the published literature
+R_0 <- 12 ##No change
 N <- 10000 ##No change
 I <- 12  ##No change
-tau <- 12  ##No change
+tau <- 12  ##Increased to maximum infectious period
 
 gamma = gamma_val(tau) ##No change
 beta = beta_val(R_0, N, gamma) ##This will change based on the value of R0
@@ -106,7 +109,7 @@ time <- seq(0, 50) ##No change
 SIR_4 <- as.data.frame(ode(y = init,
                            times = time,
                            func = sir_equations,
-                           parms = par_3
+                           parms = par_4
 ))
 
 plot_4 <- plot_graph(SIR_4)
@@ -115,3 +118,170 @@ plot_4 <- plot_graph(SIR_4)
 patchwork <- plot_1 + plot_2 + plot_3 + plot_4
 patchwork + plot_annotation(title = "Measles infectious dynamics for a range of parameter values",
                             caption = "Green = Susceptible, Red = Infectious, Blue = Recovered")
+
+##We will now use the initial model of an R0 of 12 and tau of 9 days for different starting conditions
+R_0 <- 12 ##The number of infections caused by a single introduction of a case into a susceptible population
+N <- 10000 ##The population at the beginning of the model
+I <- 12  ##The number of introduced infections
+R <- 0 ##The number of previously immune individuals
+tau <- 9 ##The number of days an individual is infectious for
+
+gamma = gamma_val(tau) ##The rate of recovery from infection per unit time is termed gamma
+beta = beta_val(R_0, N, gamma) ##The effective infectious contact rate per unit time is termed beta
+
+par <- c(
+  gamma,
+  beta
+)
+
+init <- c(
+  S = N-I,  ##The number of susceptible individuals at time point t=0 is S
+  I =   I,  ##The number of infectious individuals introduced at time point t=0 is I
+  R =   R   ##The number of recovered individuals at time point t=0 is R
+)
+
+time <- seq(0, 50) ##This represents the time step between T1 and T2
+
+##First model
+SIR_1 <- as.data.frame(ode(y = init,
+                           times = time,
+                           func = sir_equations,
+                           parms = par
+))
+
+plot_5 <- plot_graph(SIR_1)+
+  labs(title = paste("S=", init[1], "I=", I, "R=", R),
+       caption = paste("R0 =",R_0, "Tau =", tau, "days"))
+
+##Changing the starting conditions to a reduced number of initially infected individuals
+N <- 10000
+I <- 2
+R <- 0
+
+init <- c(
+  S = N-(I+R),  ##The number of susceptible individuals at time point t=0 is S
+  I =   I,  ##The number of infectious individuals introduced at time point t=0 is I
+  R =   R   ##The number of recovered individuals at time point t=0 is R
+)
+
+##Second model
+SIR_5 <- as.data.frame(ode(y = init,
+                           times = time,
+                           func = sir_equations,
+                           parms = par
+))
+
+plot_6 <- plot_graph(SIR_5)+
+  labs(title = paste("S=", init[1], "I=", I, "R=", R),
+       caption = paste("R0 =",R_0, "Tau =", tau, "days"))
+
+##Changing the starting conditions to an increased number of vaccinated individuals to current UK average (2 doses by 5) = 86.4
+N <- 10000
+I <- 12
+R <- 8640
+
+init <- c(
+  S = N-(I+R),  ##The number of susceptible individuals at time point t=0 is S
+  I =   I,  ##The number of infectious individuals introduced at time point t=0 is I
+  R =   R   ##The number of recovered individuals at time point t=0 is R
+)
+
+##Third model
+SIR_6 <- as.data.frame(ode(y = init,
+                           times = time,
+                           func = sir_equations,
+                           parms = par
+))
+
+plot_7 <- plot_graph(SIR_6)+
+  labs(title = paste("S=", init[1], "I=", I, "R=", R),
+       caption = paste("R0 =",R_0, "Tau =", tau, "days"))
+
+##Changing the starting conditions to an increased number of vaccinated individuals to level required for herd immunity = 98%
+N <- 10000
+I <- 12
+R <- 9800
+
+init <- c(
+  S = N-(I+R),  ##The number of susceptible individuals at time point t=0 is S
+  I =   I,  ##The number of infectious individuals introduced at time point t=0 is I
+  R =   R   ##The number of recovered individuals at time point t=0 is R
+)
+
+##Fourth model
+SIR_7 <- as.data.frame(ode(y = init,
+                           times = time,
+                           func = sir_equations,
+                           parms = par
+))
+
+plot_8 <- plot_graph(SIR_7)+
+  labs(title = paste("S=", init[1], "I=", I, "R=", R),
+       caption = paste("R0 =",R_0, "Tau =", tau, "days"))
+
+patchwork_2 <- plot_5 + plot_6 + plot_7 + plot_8
+patchwork_2 + plot_annotation(title = "Measles infectious dynamics for a range of parameter values",
+                            caption = "Green = Susceptible, Red = Infectious, Blue = Recovered")
+
+# Part 2 ------------------------------------------------------------------
+##The UK population in 1950 was 50380000
+##Setting model parameters
+R_0 <- 12 ##The number of infections caused by a single introduction of a case into a susceptible population
+N <- 50380000 ##The population at the beginning of the model
+I <- 3000/7  ##The number of introduced infections, this is the level of the lowest number of weekly cases/days
+R <- ((50380000/100)*91.3)-I ##The number of resistant individuals in the population
+tau <- 7 ##The number of days an individual is infectious for
+
+gamma = gamma_val(tau) ##The rate of recovery from infection per unit time is termed gamma
+beta = beta_val(R_0, N, gamma) ##The effective infectious contact rate per unit time is termed beta
+
+par_5 <- c(
+  gamma,
+  beta
+)
+
+init <- c(
+  S = N-(I+R),  ##The number of susceptible individuals at time point t=0 is S
+  I =   I,  ##The number of infectious individuals introduced at time point t=0 is I
+  R =   R   ##The number of recovered individuals at time point t=0 is R
+)
+
+time <- seq(0, 3650) ##This represents the time step between T1 and T2
+
+##First model
+SIR_8 <- as.data.frame(ode(y = init,
+                           times = time,
+                           func = sir_equations,
+                           parms = par_5
+))
+
+plot_9 <- measles_outbreaks(SIR_8)
+
+##This approximately reproduces the peak of cases from the data, however over a much greater timeline
+max(SIR_8$I)
+
+##The birth rate in the UK and 1950 is reported as 14.39 per 1000 individuals. Estimated as around 2,000 births per day
+mu <- ((14.39/1000)/365)
+
+par_6 <- c(
+  gamma,
+  beta,
+  mu
+)
+
+##First model
+SIR_9 <- as.data.frame(ode(y = init,
+                           times = time,
+                           func = sir_birth_death,
+                           parms = par_6
+))
+
+cyclical_inf <- measles_outbreaks(SIR_9)
+cyclical_sus <- measles_sus(SIR_9)
+cyclical_rec <- measles_rec(SIR_9)
+
+cyclical_inf/cyclical_rec/cyclical_sus
+
+# Part 3 ------------------------------------------------------------------
+##Using social mixing data and an age structure model
+UK_child_contact <- contact_matrix(survey = polymod, countries = c("United Kingdom"), age.limits = c(0,1,5,15), symmetric = T)
--- Miniproject_functions.R
+++ Miniproject_functions.R
@@ -17,3 +17,59 @@ sir_equations <- function(time, variables, parameters) {
     return(list(c(dS, dI, dR)))
   })
 }
+
+##Function to plot the SIR outcomes for a range of R0 and tau
+plot_graph <- function(plot_model){
+  ggplot(plot_model, aes(x = time))+
+    geom_line(aes(y = S), colour = "green")+
+    geom_line(aes(y = I), colour = "red")+
+    geom_line(aes(y = R), colour = "blue")+
+    theme_minimal()+
+    labs(title = paste("R0 =",R_0, "Tau =", tau, "days"))+
+    xlab("Time in days")+
+    ylab(paste("N=", N))+
+    theme(plot.title = element_text(size = 12))
+}
+
+##Function to plot the outbreak size
+measles_outbreaks <- function(measles_cases){
+  ggplot(measles_cases, aes(x = time))+
+    geom_line(aes(y = I), colour = "red")+
+    theme_minimal()+
+    labs(title = paste("Infectious population R0 =",R_0, "Tau =", tau, "days"))+
+    xlab("Time in days")+
+    ylab(paste("N=", N))+
+    theme(plot.title = element_text(size = 12))
+}
+
+##Function to solve the SIR differential equation when births incorporated
+sir_birth_death <- function(time, variables, parameters) {
+  with(as.list(c(variables, parameters)), {
+    dS <- (-beta * I * S) + (mu*N) - (mu*S)
+    dI <- (beta * I * S) - (gamma * I) - (mu*I)
+    dR <- (gamma * I) - (mu*R)
+    return(list(c(dS, dI, dR)))
+  })
+}
+
+##Function to plot changing susceptible population size
+measles_sus <- function(measles_cases){
+  ggplot(measles_cases, aes(x = time))+
+    geom_line(aes(y = S), colour = "green")+
+    theme_minimal()+
+    labs(title = paste("Susceptible population R0 =",R_0, "Tau =", tau, "days"))+
+    xlab("Time in days")+
+    ylab(paste("N=", N))+
+    theme(plot.title = element_text(size = 12))
+}
+
+##Function to plot changing susceptible population size
+measles_rec <- function(measles_cases){
+  ggplot(measles_cases, aes(x = time))+
+    geom_line(aes(y = R), colour = "blue")+
+    theme_minimal()+
+    labs(title = paste("Recovered population R0 =",R_0, "Tau =", tau, "days"))+
+    xlab("Time in days")+
+    ylab(paste("N=", N))+
+    theme(plot.title = element_text(size = 12))
+}
